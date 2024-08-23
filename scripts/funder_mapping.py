61c6c811a7367fa107ab6a5219d1b4abfb8a41b2
gitstart-nimhdsst <gitstart-nimhdsst@users.noreply.github.com>
2024-08-23 08:21:05 +0000

    remove duplicate

--- scripts/funder_mapping.py
+++ scripts/funder_mapping.py
@@ -6,7 +6,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 indicators_df = pd.read_csv('indicators_all.csv')
 funders_df = pd.read_csv('biomedical_research_funders.csv')
 
@@ -21,20 +20,21 @@ def data_cleaning_processing():
             '[^\w\s]', '', regex=True)  # Remove punctuation
 
 
-def founder_mapping(funder_names_acronyms):
+def funder_mapping(funder_names, funder_acronyms):
     """map founder and return a new df with new mapping"""
     try:
         output_df = pd.DataFrame(indicators_df['pmid'])
 
         # Check for funder matches and populate the output DataFrame
-        for names_acronyms in funder_names_acronyms:
+        for name in funder_names:
             # Initialize a column for each funder with False
-            output_df[names_acronyms] = False
+            output_df[name] = False
 
             # Update the column if any of the funding information columns contain the funder name
-            for column in funding_columns:
-                output_df[names_acronyms] = output_df[names_acronyms] | indicators_df[column].str.contains(
-                    names_acronyms, case=False, na=False)
+            for ind, column in enumerate(funding_columns):
+                output_df[name] = (output_df[name]
+                                   | indicators_df[column].str.contains(name, case=False, na=False)
+                                   | indicators_df[column].str.contains(funder_acronyms[ind], case=False, na=False))
 
         return output_df
 
@@ -50,14 +50,12 @@ def output_to_file(output_df: pd.DataFrame):
 
 def main():
     try:
-        funder_names_set = set(funders_df['Name'].tolist())
-        funder_acronyms = set(funders_df['Acronym'].tolist())
-
-        funder_names_acronyms = funder_names_set.union(funder_acronyms)
+        funder_names = funders_df['Name'].tolist()
+        funder_acronyms = funders_df['Acronym'].tolist()
 
         data_cleaning_processing()
 
-        output_df = founder_mapping(funder_names_acronyms)
+        output_df = funder_mapping(funder_names, funder_acronyms)
 
         output_to_file(output_df)
     except Exception as e:
