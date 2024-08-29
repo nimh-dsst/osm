47bcb2c0ff2d59c58ba44128ffa7103cf0711f0b
leej3 <johnleenimh@gmail.com>
2024-08-29 14:47:54 +0100

    llm wip

--- osm/schemas/__init__.py
+++ osm/schemas/__init__.py
@@ -1,6 +1,8 @@
 from .schemas import Client as Client
 from .schemas import Component as Component
 from .schemas import Invocation as Invocation
+from .schemas import LLMExtractorMetrics as LLMExtractorMetrics
+from .schemas import ManualAnnotationNIMHDSST as ManualAnnotationNIMHDSST
 from .schemas import PayloadError as PayloadError
 from .schemas import Quarantine as Quarantine
 from .schemas import RtransparentMetrics as RtransparentMetrics
