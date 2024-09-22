cb4d9af4e95b17afb3b2106224bc2994a103c60f
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-09-21 18:56:10 -0700

    Refactor state bootstrap to use variables and output modules

---
+++ web/deploy/terraform/modules/bootstrap/state_storage/variables.tf
@@ -0,0 +1,22 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique."
+  type        = string
+  default     = "osm-storage"
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table. Must be unique in this AWS account."
+  type        = string
+  default     = "terraform-locks"
+}
+
+variable "aws_region" {
+  description = "The AWS region used by the deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "development_environment" {
+  description = "The name of the development environment. Usually `stage` or `prod`."
+  type        = string
+}
