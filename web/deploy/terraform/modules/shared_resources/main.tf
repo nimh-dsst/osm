e8e3d312ca0d121d3394f341b5b8534d0fe560d6
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-09-23 12:46:01 -0700

    Fix linting errors

--- web/deploy/terraform/modules/shared_resources/main.tf
+++ web/deploy/terraform/modules/shared_resources/main.tf
@@ -1,23 +1,41 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# tflint-ignore: terraform_unused_declarations
 variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
+  type        = string
 }
 
-
+# tflint-ignore: terraform_unused_declarations
 variable "s3_bucket" {
   description = "S3 bucket for Terraform state"
   default     = "osm-storage"
+  type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "dynamodb_table" {
   description = "DynamoDB table for Terraform state locking"
   default     = "terraform-locks"
+  type        = string
 }
+
+# tflint-ignore: terraform_unused_declarations
 variable "ssh_port" {
   description = "Non-standard port for SSH"
   default     = 22
+  type        = number
 }
-
 
 # VPC
 resource "aws_vpc" "main" {
