31dd6a0d343d13fa4b06a5ea9935db5b12e84fbb
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-09-23 22:22:26 -0700

    Move all state bootstrap files to a single directory

--- web/deploy/terraform/production/bootstrap/state_storage/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "environment" {
-  description = "The name of the development environment. Usually `stage` or `prod`. Defaults to `prod`"
-  type        = string
-  default     = "prod"
-}
--- web/deploy/terraform/staging/bootstrap/state_storage/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "environment" {
-  description = "The name of the development environment. Usually `stage` or `prod`. Defaults to `stage`"
-  type        = string
-  default     = "stage"
-}
