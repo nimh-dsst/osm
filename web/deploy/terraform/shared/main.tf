ce6440de1ba564d78eab53889c174dfd8312622a
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-10-01 00:10:17 -0700

    Add ECR configuration

--- web/deploy/terraform/shared/main.tf
+++ web/deploy/terraform/shared/main.tf
@@ -21,3 +21,8 @@ module "networking" {
   source      = "../modules/networking/"
   environment = var.environment
 }
+
+module "ecr" {
+  source      = "../modules/ecr/"
+  environment = var.environment
+}
