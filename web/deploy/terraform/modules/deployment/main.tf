28cc1f6939d570f9a2306b027548abc81afe029e
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-10-15 17:37:42 -0700

    Move script to separate file

--- web/deploy/terraform/modules/deployment/main.tf
+++ web/deploy/terraform/modules/deployment/main.tf
@@ -45,17 +45,7 @@ resource "aws_instance" "staging" {
     Name = var.environment
   }
 
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt install -y curl
-    apt-get install -y docker.io
-    curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-    chmod a+x /usr/local/bin/docker-compose
-    systemctl restart sshd
-    systemctl start docker
-    systemctl enable docker
-              EOF
+  user_data = file("${path.module}/scripts/install-docker.sh")
 }
 
 resource "aws_eip" "staging" {
