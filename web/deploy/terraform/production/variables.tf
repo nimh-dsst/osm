711e871fcad08cdc726f29e9b855daed8b4099bc
Marcelo Villa <mvilla@quansight.com>
2024-10-23 20:57:14 +0200

    Use larger instance for production

--- web/deploy/terraform/production/variables.tf
+++ web/deploy/terraform/production/variables.tf
@@ -1,11 +1,11 @@
 variable "environment" {
-  description = "The name of the environment. Usually `prod`."
+  description = "The name of the environment. Usually `prod`"
   default     = "prod"
   type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.nano"
+  default     = "t3.large"
   type        = string
 }
