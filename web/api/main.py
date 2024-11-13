0f0881fdeecff8ed9a5f8d7fe6b2b155c95fe139
leej3 <johnleenimh@gmail.com>
2024-11-13 11:23:41 +0000

    add healthcheck for api service

--- web/api/main.py
+++ web/api/main.py
@@ -5,9 +5,10 @@ Sets up a web API for uploading osm metrics to a centralized database
 import os
 
 import motor.motor_asyncio
-from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile, status
 from fastapi.responses import JSONResponse
 from odmantic import AIOEngine, ObjectId
+from pydantic import BaseModel
 
 from osm.schemas import Invocation, PayloadError, Quarantine
 
@@ -15,6 +16,33 @@ app = FastAPI()
 dburi = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
 client = motor.motor_asyncio.AsyncIOMotorClient(dburi)
 engine = AIOEngine(client=client, database="osm")
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
 
 
 @app.put("/upload/", response_model=Invocation)
